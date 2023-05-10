b7214054588a44056a4323c93de4bbadc9fc5098
Odalmer Pimentel <48610363+odalmer@users.noreply.github.com>
2023-05-09 21:21:05 -0400

    first commit, no styling

---
+++ index.js
@@ -0,0 +1,76 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const mongoose = require("mongoose");
+const methodOverride = require("method-override");
+const Product = require("./models/product");
+const categories = ["fruit", "vegetable", "dairy"];
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/farmStand")
+  .then(() => {
+    console.log("MONGO CONNECTION OPEN!!");
+  })
+  .catch((err) => {
+    console.log("MONGO CONNECTION ERROR!!!");
+    console.log(err);
+  });
+
+app.set("views", path.join(__dirname, "/views"));
+app.set("view engine", "ejs");
+
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.use(methodOverride("_method")); // override with POST having ?_method=DELETE
+
+app.get("/products", async (req, res) => {
+  const { category } = req.query;
+  if (category) {
+    const products = await Product.find({ category });
+    res.render("products/index", { products, category });
+  } else {
+    const products = await Product.find({});
+    res.render("products/index", { products, category: "All" });
+  }
+});
+
+app.get("/products/new", (req, res) => {
+  res.render("products/new", { categories });
+});
+
+app.post("/products", async (req, res) => {
+  const { name, price, category } = req.body;
+  const newProduct = new Product({ name, price, category });
+  await newProduct.save();
+  res.redirect(`/products/${newProduct._id}`);
+});
+
+app.get("/products/:id", async (req, res) => {
+  const { id } = req.params;
+  const product = await Product.findById(id);
+  res.render("products/show", { product });
+});
+
+app.get("/products/:id/edit", async (req, res) => {
+  const { id } = req.params;
+  const product = await Product.findById(id);
+  res.render("products/edit", { product, categories });
+});
+
+app.put("/products/:id", async (req, res) => {
+  const { id } = req.params;
+  const product = await Product.findByIdAndUpdate(id, req.body, {
+    runValidators: true,
+    new: true,
+  });
+  res.redirect(`/products/${product._id}`);
+});
+
+app.delete("/products/:id", async (req, res) => {
+  const { id } = req.params;
+  await Product.findByIdAndDelete(id);
+  res.redirect("/products");
+});
+
+app.listen(3000, () => {
+  console.log("Listening on port 3000");
+});
